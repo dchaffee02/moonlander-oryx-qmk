234637709b734d54f527f501cd6fc32634499c85
dchaffee02 <dchaffee02@gmail.com>
2025-10-21 08:45:19 -0600

    Update keymap.c
    mapped Tai-Hao key to e (0x0065) to test formatting

--- nDzR0/keymap.c
+++ nDzR0/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_KP_ASTERISK, KC_KP_SLASH,    
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                                                          KC_NO,          KC_BSPC,        KC_KP_4,        KC_KP_5,        KC_KP_6,        KC_KP_MINUS,    KC_DELETE,      
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_KP_1,        KC_KP_2,        KC_KP_3,        KC_KP_PLUS,     KC_KP_ENTER,    
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          RGB_MODE_FORWARD,                                                                                                KC_NO,          KC_KP_0,        KC_KP_COMMA,    KC_KP_DOT,      KC_KP_EQUAL,    KC_NO,          
+    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          RGB_MODE_FORWARD,                                                                                                KC_NO,          KC_KP_0,        KC_KP_COMMA,    KC_KP_DOT,      KC_KP_EQUAL,    UC(0x0065),          
     RGB_SPD,        RGB_SPI,        TOGGLE_LAYER_COLOR,                KC_NO,          KC_NO,          KC_NO
   ),
 };
